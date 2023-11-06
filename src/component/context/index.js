ab905462c2a03067d5534d56f59d0a01580a34b1
anas599 <anas599@yahoo.com>
2023-11-06 17:41:15 +0300

    add && remove player in context

--- src/component/context/index.js
+++ src/component/context/index.js
@@ -6,11 +6,25 @@ class MyProvider extends Component {
     player: [],
     result: "",
   };
+  addPlayer = (player) => {
+    this.setState((prevState) => {
+      return {
+        player: [...prevState.player, player],
+      };
+    });
+  };
+  removePlayer = (player) => {
+    let newPlayer = this.state.player;
+    newPlayer.splice(newPlayer.indexOf(player), 1);
+    this.setState({ player: newPlayer });
+  };
   render() {
     return (
       <MyContext.Provider
         value={{
           state: this.state,
+          addPlayer: this.addPlayer,
+          removePlayer: this.removePlayer,
         }}
       >
         {this.props.children}
